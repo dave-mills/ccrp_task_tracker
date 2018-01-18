316d618517aff97cd34778c84f2b4e1acd05764d
Dave Mills <d.e.mills@stats4sd.org>
2018-01-18 13:31:45 +0000

    setting ccrp-tasks script to only run on single page to avoid conflicts with other plugins (fix to our-team page)

--- ccrp_task_tracker.php
+++ ccrp_task_tracker.php
@@ -38,32 +38,35 @@ add_action('wp_enqueue_scripts','init_taskTables');
 
 function init_taskTables() {
 
-  wp_enqueue_style( 'ccrp_tasks-style',plugin_dir_url( __FILE__ ) . "css/ccrp_tasks.css", time() );
 
-  wp_register_script( 'ccrp_tasks-script', plugin_dir_url( __FILE__ ) . '/js/ccrp_tasks.js', array( 'jquery','dt-script','dataTables-editor-script' ), time(), true );
-  wp_enqueue_script( 'ccrp_tasks-script');
+  if(is_page_template('page-template-ccrp_tasks.php')) {
+    wp_enqueue_style( 'ccrp_tasks-style',plugin_dir_url( __FILE__ ) . "css/ccrp_tasks.css", time() );
 
-  //select 2 scripts: 
+    wp_register_script( 'ccrp_tasks-script', plugin_dir_url( __FILE__ ) . '/js/ccrp_tasks.js', array( 'jquery','dt-script','dataTables-editor-script' ), time(), true );
+    wp_enqueue_script( 'ccrp_tasks-script');
+  }
+  
+    //select 2 scripts: 
+    //
+    wp_enqueue_style('select2-style',"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css","4.0.6");
+    wp_enqueue_script('select2-script',"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js",array('jquery'),"4.0.6",true);
+
+  //add mustache support
   //
-  wp_enqueue_style('select2-style',"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css","4.0.6");
-  wp_enqueue_script('select2-script',"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js",array('jquery'),"4.0.6",true);
-
-//add mustache support
-//
-  wp_enqueue_script('mustache-script',plugin_dir_url(__FILE__) . '/js/mustache.js/mustache.min.js',array('jquery'),"4.0.6",true);
+    wp_enqueue_script('mustache-script',plugin_dir_url(__FILE__) . '/js/mustache.js/mustache.min.js',array('jquery'),"4.0.6",true);
 
 
-  //Setup the parameters for "localising" the javascript. (i.e. passing values into the javascript)
-  $params = array(
-    //file path for the ajax files we need to reference in the javascript.
-    'editorurl' => plugin_dir_url(__FILE__) . 'ajax',
-    //the ajax url for a propoer wordpress ajax call;
-    'ajaxurl' => admin_url( 'admin-ajax.php' ),
-    // create a 'nonce' to properly authenticate any ajax requests. 
-    'pa_nonce' => wp_create_nonce('pa_nonce'),
-    'current_user' => wp_get_current_user()->ID
-  );
+    //Setup the parameters for "localising" the javascript. (i.e. passing values into the javascript)
+    $params = array(
+      //file path for the ajax files we need to reference in the javascript.
+      'editorurl' => plugin_dir_url(__FILE__) . 'ajax',
+      //the ajax url for a propoer wordpress ajax call;
+      'ajaxurl' => admin_url( 'admin-ajax.php' ),
+      // create a 'nonce' to properly authenticate any ajax requests. 
+      'pa_nonce' => wp_create_nonce('pa_nonce'),
+      'current_user' => wp_get_current_user()->ID
+    );
 
-  //localise!
-  wp_localize_script('ccrp_tasks-script','vars',$params);
+    //localise!
+    wp_localize_script('ccrp_tasks-script','vars',$params);
 }
